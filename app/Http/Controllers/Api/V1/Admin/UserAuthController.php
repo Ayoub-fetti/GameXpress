8105b7394979b1cb21b897ac01a4cdc4d7cf67f8
Ayoub-fetti <ayoubfetti3@gmail.com>
2025-03-19 11:15:18 +0000

    merge donnnnnne

--- app/Http/Controllers/Api/V1/Admin/UserAuthController.php
+++ app/Http/Controllers/Api/V1/Admin/UserAuthController.php
@@ -32,39 +32,6 @@ class UserAuthController extends Controller
         return response()->json(['message' => 'User registered successfully'], 201);
     }
 
-    // public function login(Request $request)
-    // {
-    //     $request->validate([
-    //         'email' => 'required|string|email',
-    //         'password' => 'required|string',
-    //     ]);
-
-    //     $user = User::where('email', $request->email)->first();
-
-    //     if (! $user || ! Hash::check($request->password, $user->password)) {
-    //         throw ValidationException::withMessages([
-    //             'email' => ['The provided credentials are incorrect.'],
-    //         ]);
-    //     }
-
-    //     if ($user->hasAnyRole(['user'])) {
-    //         $token = $user->createToken('user-token')->plainTextToken;
-    //         return response()->json([
-    //         'message' => 'login success to user',
-    //         'token' => $token
-    //         ], 200);
-    //     }
-
-    //     if (!$user->hasAnyRole(['super_admin', 'product_manager', 'user_manager'])) {
-    //         return response()->json(['message' => 'You do not have permission to login'], 403);
-    //     }
-
-
-
-    //     $token = $user->createToken('user-token')->plainTextToken;
-
-    //     return response()->json(['token' => $token], 200);
-    // }
     public function login(Request $request)
 {
     $request->validate([
